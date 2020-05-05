05918501eefcb2bcfc39701f4bf830be8bed7e83
japerry911 <jack.e.perry@outlook.com>
2020-05-05 09:15:06 -0600

    Added in basic routes and router

---
+++ Frontend/src/Router/Routes.js
@@ -0,0 +1,44 @@
+import { Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Shop from '../pages/Shop';
+import SignIn from '../pages/SignIn';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route 
+                exact
+                path='/'
+                component={Home}
+            />
+
+            <Route
+                exact
+                path='/about'
+                component={About}
+            />
+
+            <Route 
+                exact
+                path='/contact'
+                component={Contact}
+            />
+
+            <Route 
+                exact
+                path='/shop'
+                component={Shop}
+            />
+
+            <Route 
+                exact 
+                path='/sign-in'
+                component={SignIn}
+            />
+        </Switch>
+    );
+};
+
+export default Routes;
---
+++ Frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <d1>
+            <h1>
+                About
+            </h1>
+        </d1>
+    );
+};
+
+export default About;
