2224d44670e68f5f1f00505f2f77b1978777063c
japerry911 <jack.e.perry@outlook.com>
2020-05-09 10:56:03 -0600

    Goods table created and migrated, food seeded

--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Good.destroy_all
+
+Good.create(
+    name: 'Blue Buffalo Life Protection Formula Adult Chicken & Brown Rice Recipe Dry Dog Food',
+    description: 'Great Dog Food that is Skylord-Approved. This food is made with Chicken and Brown Rice. Enjoy!',
+    price: 49.98,
+    card_image: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/71hs9GihZUL._AC_SY606_.jpg',
+    detail_image1: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/5150022_alt3.jpeg',
+    detail_image2: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/5066963_alt4.jpeg',
+    product_type: 'Food'
+)
+
+Good.create(
+    name: 'Taste of the Wild High Prairie Grain-Free Dry Dog Food',
+    description: 'Skylord-Approved food that comes from the wild! This is made with NO grain and is from the High Prarie. Enjoy!',
+    price: 48.99,
+    card_image: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/taste_of_the_wild_high_prairie_roasted_bison__25492.1521571508.jpg',
+    detail_image1: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/49064_PT3._AC_SL320_V1587650476_.jpg',
+    detail_image2: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/Taste-of-the-Wild-High-Prairie-Canine-Recipe-with-Roasted-Bison-and-Roasted-Venison-Grain-Free-Dry-Dog-Food-28-Lb-P2.jpeg',
+    product_type: 'Food'
+)
+
+Good.create(
+    name: 'Purina Pro Plan Focus Adult Sensitive Skin & Stomach Salmon & Rice Formula Dry Dog Food',
+    description: 'Does your dog have a sensitive stomach? Do they constantly feel sick? Well look no futher, Skylord-Approved Purina Pro Plan food is now here
+    and does not hurt dog stomachs. Enjoy!',
+    price: 47.98,
+    card_image: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/81ohh0jt2qL._AC_SL1500_.jpg',
+    detail_image1: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/5266806_alt2.jpeg',
+    detail_image2: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/2986221-center-5.webp',
+    product_type: 'Food'
+)
+
+Good.create(
+    name: 'Blue Buffalo Homestyle Recipe Chicken Dinner with Garden Vegetables & Brown Rice Canned Dog Food, 12.5-oz, case of 12',
+    description: 'Is your dog a wild animal? Well then you have to try the Skylord-Approved Chicken Dinner with Garden Vegetables and Brown Rice food!
+    It has all of the antioxitants that your dog needs to thrive. Enjoy!',
+    price: 18.00,
+    card_image: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/84158_MAIN._AC_SL1500_V1572895457_.jpg',
+    detail_image1: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/64620_PT8._AC_SL1500_V1499959911_.jpg',
+    detail_image2: 'https://skylord-depot.s3.us-east-2.amazonaws.com/Shop/Food/download.jpeg',
+    product_type: 'Food'
+)
+
