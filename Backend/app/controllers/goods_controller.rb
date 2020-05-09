dfbc5c0e78c3340096c51cbb829773f52ab0adb3
japerry911 <jack.e.perry@outlook.com>
2020-05-09 11:30:19 -0600

    Added in index/show routes & actions, ready-for-merge

--- Backend/app/controllers/goods_controller.rb
+++ Backend/app/controllers/goods_controller.rb
@@ -1,2 +1,13 @@
 class GoodsController < ApplicationController
+    def index
+        @goods = Good.all
+
+        render json: { goods: @goods }
+    end
+
+    def show 
+        @good = Good.find(params[:id])
+
+        render json: { good: @good }
+    end
 end
