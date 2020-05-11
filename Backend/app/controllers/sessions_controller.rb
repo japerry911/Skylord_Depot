68c64e84ccda2a17c2aa1eb522db8a78a90bd4ba
japerry911 <jack.e.perry@outlook.com>
2020-05-11 11:44:06 -0600

    Added bcrypt authentication and login endpoint to backend

---
+++ Backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+    def login 
+        @user = User.find_by(username: user_params[:username])
+
+        if !@user
+            render json: { message: 'Wrong username' }, status: :unauthorized
+        else
+            if @user.authenticate(user_params[:password])
+                payload = {
+                    user_id: @user.id
+                }
+                secret_key = Rails.application.secret_key_base
+
+                token = JWT.encode(payload, secret_key)
+
+                render json: { user: { username: @user.username, token: token }}
+            else
+                render json: { message: 'Wrong password' }, status: :unauthorized
+            end
+        end
+    end
+
+    private 
+
+        def user_params
+            params.require(:user).permit([:username, :password])
+        end
+end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :goods, only: [:index, :show]
+  post '/login', to: 'sessions#login'
 end
