62b7c8f3bec8944e8088288ba20f303d7a356ba4
japerry911 <jack.e.perry@outlook.com>
2020-05-12 12:22:01 -0600

    bug fixed where 0 quantity items were showing in cart

--- Backend/app/controllers/order_items_controller.rb
+++ Backend/app/controllers/order_items_controller.rb
@@ -1,17 +1,23 @@
 class OrderItemsController < ApplicationController
     def create
         order_item = OrderItem.find_by(user: order_item_params[:user], good: order_item_params[:good])
-        
-        if order_item
-            @return_order_item = order_item.update(quantity: order_item_params[:quantity])
-        else
-            user = User.find(order_item_params[:user])
-            good = Good.find(order_item_params[:good])
 
-            @return_order_item = OrderItem.create(user: user, good: good, quantity: order_item_params[:quantity])
+        if order_item_params[:quantity] == "0"
+            order_item.destroy
+            
+            render status: :ok
+        else 
+            if order_item
+                @return_order_item = order_item.update(quantity: order_item_params[:quantity])
+            else
+                user = User.find(order_item_params[:user])
+                good = Good.find(order_item_params[:good])
+
+                @return_order_item = OrderItem.create(user: user, good: good, quantity: order_item_params[:quantity])
+            end
+
+            render status: :ok
         end
-
-        render status: :ok
     end
 
     private
