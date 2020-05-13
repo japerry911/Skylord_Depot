1d6519eb4b93369def4f70faa514bddadba21fae
japerry911 <jack.e.perry@outlook.com>
2020-05-13 17:08:50 -0600

    boilerplate & added in heroheader

--- Frontend/src/pages/OrderConfirmation.js
+++ Frontend/src/pages/OrderConfirmation.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import HeroHeader from '../components/HeroHeader';
 
 const OrderConfirmation = () => {
     return (
-        <div>
-            <h1>ORDER CONFIRMATION</h1>
+        <div className='order-confirmation-main-div'>
+            <HeroHeader
+                headerText='Order Confirmation'
+                imageUrl='https://skylord-depot.s3.us-east-2.amazonaws.com/OrderConfirmation/744CC310-352B-4DA0-B40B-45467A63F47B_1_201_a.jpeg'
+            />
         </div>
     );
 };
