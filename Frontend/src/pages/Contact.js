157b72f1525ce99d19f8bc18df1db971a8f604c8
japerry911 <jack.e.perry@outlook.com>
2020-05-14 15:26:18 -0600

    general layout draft finished

--- Frontend/src/pages/Contact.js
+++ Frontend/src/pages/Contact.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 
 const Contact = () => {
+    const [validated, setValidated] = useState(false);
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        setValidated(email && subject && message);
+    }, [email, subject, message]);
+
     return (
         <div className='contact-main-div'>
-            <h1>Contact Page</h1>
+            <div className='content-div'>
+                <h3 className='contact-header-h3'>Contact Sky & Jack!</h3>
+                <form>
+                    <h4 className='contact-header-h4'>Contact Form</h4>
+                    <Divider className='divider' />
+                    <TextField 
+                        className='contact-input'
+                        label='Email'
+                        type='email'
+                        onChange={newEmail => setEmail(newEmail.target.value)} 
+                    />
+                    <TextField 
+                        className='contact-input'
+                        label='Subject'
+                        onChange={newSubject => setSubject(newSubject.target.value)} 
+                    />
+                    <TextareaAutosize 
+                        className='contact-textarea'
+                        placeholder='Your message...'
+                        rowsMin={5}
+                        rowsMax={10}
+                        onChange={newMessage => setMessage(newMessage.target.value)} 
+                    />
+                    <Divider className='divider' />
+                    <Button className='btn'>
+                        Submit Message
+                    </Button>
+                </form>
+            </div>
         </div>
     );
 };
