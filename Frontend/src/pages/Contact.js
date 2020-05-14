26650f591700005df151db6cd300cb4cb3037111
japerry911 <jack.e.perry@outlook.com>
2020-05-14 15:43:08 -0600

    toast added, and handleSubmit finished, ready-for-merge

--- Frontend/src/pages/Contact.js
+++ Frontend/src/pages/Contact.js
@@ -3,12 +3,19 @@ import TextField from '@material-ui/core/TextField';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
+import railsServer from '../api/railsServer';
+import { useDispatch } from 'react-redux';
+import { handleOpen } from '../redux/actions/toastsActions';
+import Spinner from '../components/Spinner';
 
 const Contact = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const [validated, setValidated] = useState(false);
     const [email, setEmail] = useState('');
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
         setValidated(email && subject && message);
@@ -16,10 +23,28 @@ const Contact = () => {
 
     const handleSubmit = event => {
         event.preventDefault();
+        setIsLoading(true);
+
+        railsServer.post('contact-send-email', { email_options: { date: new Date(), email, subject, message }}).then(
+            () => {
+                dispatch(handleOpen({ type: 'success', message: 'Message successfully sent.' }));
+                setEmail('');
+                setSubject('');
+                setMessage('');
+                setIsLoading(false);
+            },
+            () => dispatch(handleOpen({ type: 'error', message: 'Message sending error. Try again later.' }))
+        );
     };
 
     return (
         <div className='contact-main-div'>
+            {isLoading
+            ?
+            <div className='spinner-div'>
+                <Spinner />
+            </div>
+            :
             <div className='content-div'>
                 <h3 className='contact-header-h3'>Contact Sky & Jack!</h3>
                 <form onSubmit={handleSubmit}>
@@ -48,7 +73,7 @@ const Contact = () => {
                         Submit Message
                     </Button>
                 </form>
-            </div>
+            </div>}
         </div>
     );
 };
