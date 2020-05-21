a68e6de0291001d74fa3305f85c989f49bc58003
japerry911 <jack.e.perry@outlook.com>
2020-05-21 07:49:26 -0600

    Current Deployed Version - main edit: added in cart option to footer when authed

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import { SocialIcon } from 'react-social-icons';
 import { Link } from 'react-router-dom';
 import { NonAuth, Auth } from '../Router/routesArrays';
@@ -6,12 +6,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { authLogout } from '../redux/actions/authActions';
 
 const Footer  = () => {
+    const [totalItemCount, setTotalItemCount] = useState(0);
+
     const loggedIn = useSelector(state => state.auth.loggedIn);
+    const cartItems = useSelector(state => state.auth.cart);
     const dispatch = useDispatch();
 
     const handleLogout = () => {
         dispatch(authLogout());
     };
+
+    useEffect(() => {
+        setTotalItemCount(cartItems.reduce((accumulator, currentValue) => accumulator + currentValue.quantity, 0));
+    }, [cartItems]);
 
     return (
         <footer className='main-footer'>
@@ -46,6 +53,7 @@ const Footer  = () => {
                                     <li key={index}><Link to={routeObject.path}>{routeObject.title}</Link></li>
                                 );
                             })}
+                            <li><Link to='/cart'>Cart (<span>{totalItemCount}</span>)</Link></li>
                             <li className='logout-list-item' onClick={handleLogout}>Sign Out</li>
                         </Fragment>
                         }
